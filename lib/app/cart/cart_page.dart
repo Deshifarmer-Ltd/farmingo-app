2cca746e965e89730a4156fa6239529d5ff81ca1
NafiuNoman <nafiunoman@gmail.com>
2024-12-18 17:58:16 +0600

    cart page ongoing
    counter need to add

--- lib/app/cart/cart_page.dart
+++ lib/app/cart/cart_page.dart
@@ -11,14 +11,66 @@ class CartPage extends GetView<CommonController> {
       body: SafeArea(
         child: Obx(() {
           return controller.cartItemList.isNotEmpty
-              ? ListView.builder(itemBuilder: (ctx, i) {
-                  var item = controller.cartItemList.elementAt(i);
+              ? ListView.builder(
+                  itemBuilder: (ctx, i) {
+                    var item = controller.cartItemList.elementAt(i);
 
-                  return ListTile(
-                    leading: Text(item.product.name),
-                    trailing: Text(item.count.value.toString()),
-                  );
-                },itemCount: controller.cartItemList.length,)
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.2,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.green.shade200,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(5))),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: InkWell(
+                                  onTap: () {},
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      // Optional: For rounded corners
+                                      child: item.product.image != null
+                                          ? Image.network(
+                                              item.product.image!,
+
+                                              fit: BoxFit.cover,
+                                              // errorBuilder: (ctx, obj, st) {
+                                              //   return Image.asset('name');
+                                              // }, // Makes the image fill the container
+                                            )
+                                          : Image.asset(
+                                              'assets/images/no_image.png',
+                                              height: 150 - 10,
+                                              fit: BoxFit.cover,
+                                            )),
+                                ),
+                              ),
+                              Expanded(flex: 7, child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(item.product.name,overflow: TextOverflow.ellipsis,style: TextStyle()),
+                                  Text(' ৳ ${item.product.price.toString()} each',style: TextStyle(color: Colors.grey),),
+                                  Text(' Total:${item.product.weight*item.count.value} ${item.product.measurement}',style: TextStyle(color: Colors.grey)),
+                                  Text(' price: ৳ ${item.product.price*item.count.value}',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.green)),
+                                ],
+                              ))
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: controller.cartItemList.length,
+                )
               : Text('Cart Is empty');
         }),
       ),
